b3142ba30c1bb1bd19619f56794416a1edbf5b95
Blake Robert Mills <blake@Blakes-Air.ent.ds.gsa.gov>
2023-09-20 16:11:57 -0400

    Palette Update

--- Scripts/Plots.R
+++ Scripts/Plots.R
@@ -1,5 +1,11 @@
 library(ggplot2)
+library(sysfonts)
+library(tidyverse)
+library(magick)
+library(cowplot)
+library(ggstream)
 library(showtext)
+library(colorblindcheck)
 library(ggstream)
 font <- "Advent Pro"
 font_add_google("Advent Pro")
@@ -99,81 +105,10 @@ for(i in names(Colors)){
   print(i)
   
 }
-
-
-print.palette <- function(x, ...) {
-  n <- length(x)
-  lab <- attr(x, "name")
-  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
-  on.exit(par(old))
-  
-  image(1:n, 1, as.matrix(1:n), col = x,
-        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
-  
-  rect(0, 0.92, n + 1, 1.08, col = rgb(1, 1, 1, 0.8), border = NA)
-  text((n + 1) / 2, 1, labels = substitute(bold(lab)), cex = 4, family = "Advent Pro")
-}
-
-moma.colors <- function(palette_name, n, type = c("discrete", "continuous"), direction = c(1, -1), override.order=FALSE) {
-  
-  `%notin%` <- Negate(`%in%`)
-  
-  palette <- Colors[[palette_name]]
-  
-  if (is.null(palette)|is.numeric(palette_name)){
-    stop("Palette does not exist.")
-  }
-  
-  if (missing(n)) {
-    n <- length(palette[[1]])
-  }
-  
-  if (missing(direction)) {
-    direction <- 1
-  }
-  
-  if (direction %notin% c(1, -1)){
-    stop("Direction not valid. Please use 1 for standard palette or -1 for reversed palette.")
-  }
-  
-  if (missing(type)) {
-    if(n > length(palette[[1]])){type <- "continuous"}
-    else{type <- "discrete"}
-  }
-  
-  type <- match.arg(type)
-  
-  
-  if (type == "discrete" && n > length(palette[[1]])) {
-    stop("Number of requested colors greater than what discrete palette can offer, \n use continuous instead.")
-  }
-  
-  continuous <-  if(direction==1){grDevices::colorRampPalette(palette[[1]])(n)
-  }else{
-    grDevices::colorRampPalette(rev(palette[[1]]))(n)}
-  
-  discrete <- if(direction==1 & override.order==FALSE){
-    palette[[1]][which(palette[[2]] %in% c(1:n)==TRUE)]
-  }else if(direction==-1 & override.order==FALSE){
-    rev(palette[[1]][which(palette[[2]] %in% c(1:n)==TRUE)])
-  } else if(direction==1 & override.order==TRUE){
-    palette[[1]][1:n]
-  } else{
-    rev(palette[[1]])[1:n]
-  }
-  
-  out <- switch(type,
-                continuous = continuous,
-                discrete = discrete
-  )
-  structure(out, class = "palette", name = palette_name)
-  
-}
-  
   
 
 for(i in names(Colors)){
-  png(paste("~/Desktop/MoMA Pals/", i, ".png"))
+  png(paste("~/Desktop/Pals/", i, ".png", sep=""))
   print(moma.colors(i))
   dev.off()
 }
@@ -222,15 +157,13 @@ for(i in names(Colors)){
 
 for(i in names(Colors)){
   Mosaic(i)
-  ggsave(paste("~/Desktop/Mosaics/", i, ".png", sep=""))
 }
 
 
 for(i in names(Colors)){
   two <- image_read(paste("~/Desktop/Pals/", i, ".png", sep=""))
-  three <- image_read(paste("~/Desktop/Mosaics/", i, ".png", sep=""))
-  one <- image_read(paste("~/Desktop/Art/", i, ".png", sep=""))
-  xxx <- image_montage(c(one, two, three), "1000x1000", bg="white", tile=3)
+  three <- image_read(paste("~/Desktop/Current/", i, "Mos.png", sep=""))
+  xxx <- image_montage(c(two, three), "1000x1000", bg="white", tile=2)
   image_write(xxx, path=paste("~/Desktop/Complete/", i, ".png", sep=""), format="png")
 }
 
@@ -240,3 +173,31 @@ xxx <- image_montage(c(one, two, three), "1000x1000", bg="white", tile=3)
 image_write(xxx, path="~/Desktop/Montah.png", format="png")
 
 
+
+
+palette_plot(moma.colors("Liu"))
+palette_check(moma.colors("Liu"))
+palette_dist(moma.colors("Liu"))
+## Colorblind Checking 
+
+CBTotal <- data.frame() 
+
+for(pal_nam in names(Colors)){
+CBCheck <- palette_check(moma.colors(pal_nam)) %>%
+  as.data.frame %>%
+  select(name, min_dist) %>%
+  pivot_wider(names_from=name,
+              values_from = min_dist) %>%
+  mutate(pal = pal_nam)
+
+CBTotal <- rbind(CBTotal, CBCheck)
+
+png(filename = paste("~/Desktop/CB Moma/", pal_nam ,".png", sep=""))
+palette_plot(moma.colors(pal_nam))
+dev.off()
+
+
+}
+
+CBTotal$MinName <-  names(CBTotal)[apply(CBTotal, MARGIN = 1, FUN = which.min)]
+CBTotal$Min <- apply(CBTotal, MARGIN = 1, FUN = min)
